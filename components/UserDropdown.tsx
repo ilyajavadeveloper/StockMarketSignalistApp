c02ef113e3c3d675588f66ed4082f62bdb8bcdde
Your Name <you@example.com>
2026-07-05 12:04:11 +0300

    user dropdown menu

--- components/UserDropdown.tsx
+++ components/UserDropdown.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+'use client'
+
 import React from 'react'
 import { Button } from "@/components/ui/button"
 import {
@@ -11,11 +13,31 @@ import {
     DropdownMenuSeparator,
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
+import {Avatar,AvatarFallback,AvatarImage} from "@/components/ui/avatar";
+import {useRouter} from "next/navigation";
+
 const UserDropdown = () => {
+    const router = useRouter()
+
+    const handleSignOut = async () => {
+
+        router.push('/sign-in')
+    }
+
+    const user={name:"John",email:'hvost264@gmail.com'}
+
+
     return (
         <DropdownMenu>
-            <DropdownMenuTrigger>
-                <Button variant="outline">Open</Button>
+            <DropdownMenuTrigger asChild>
+                <Button variant="ghost" className='flex items-center gap-3 text-gray-4
+                hover:text-yellow-400'>
+                <Avatar>
+                    <AvatarImage src="https://github.com/shadcn.png" />
+                    <AvatarFallback>CN</AvatarFallback>
+
+                </Avatar>
+                </Button>
             </DropdownMenuTrigger>
             <DropdownMenuContent>
                 <DropdownMenuGroup>
